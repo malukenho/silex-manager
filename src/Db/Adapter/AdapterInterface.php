5f0f406d2d7d31e5fce3e62203a316a42b4e5a7b
Jefersson Nathan <admin@phpse.net>
2015-05-24 03:33:43 -0300

    Update interface with methods used to work with silex manage

--- src/Db/Adapter/AdapterInterface.php
+++ src/Db/Adapter/AdapterInterface.php
@@ -1,7 +1,15 @@
 <?php
-/**
- * AdapterInterface.php
- *
- * @package silex-manager
- * @author  Jefersson Nathan <malukenho@phpse.net>
- */
+
+namespace Manager\Db\Adapter;
+
+use Manager\Config\Node;
+
+interface AdapterInterface
+{
+    public function whereLike($name, $value);
+    public function fetch($query, $column = null);
+    public function fetchAll($sql, $params = []);
+    public function fetchByConfig(Node $config, $pagination);
+    public function limit($total, $itemPerPage, $page);
+    public function count(Node $config);
+}
