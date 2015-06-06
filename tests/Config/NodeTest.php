5aba5ec031557b45015b1fe63d24259edad51be3
Jefersson Nathan <admin@phpse.net>
2015-06-06 03:42:34 -0300

    Improve tests for Config\Node class

--- tests/Config/NodeTest.php
+++ tests/Config/NodeTest.php
@@ -6,9 +6,22 @@ use Manager\Config\Node;
 use Manager\Exception\MissingConfigException;
 use Silex\Application;
 
+/**
+ * Tests for {@see \Manager\Config\Node}
+ *
+ * @author Jefersson Nathan <malukenho@phpse.net>
+ *
+ * @group  Unitary
+ * @covers \Manager\Config\Node
+ */
 class NodeTest extends \PHPUnit_Framework_TestCase
 {
-    public function testCheckForTheConfigKey()
+    /**
+     * @dataProvider getWrongConfiguration
+     *
+     * @param $wrongConfig
+     */
+    public function testCheckForTheConfigKey($wrongConfig)
     {
         /** @var \Silex\Application||PHPUnit_Framework_MockObject_MockObject $appMock */
         $appMock = $this->getMockBuilder(Application::class)
@@ -17,10 +30,87 @@ class NodeTest extends \PHPUnit_Framework_TestCase
 
         $appMock->expects($this->once())
             ->method('offsetExists')
-            ->with('manager-config');
+            ->with('manager-config')
+            ->willReturn(true);
+
+        $appMock->expects($this->any())
+            ->method('offsetGet')
+            ->with('manager-config')
+            ->willReturn($wrongConfig);
 
         $this->setExpectedException(MissingConfigException::class);
+        new Node($appMock, 'dummy', 'index');
+    }
 
-        new Node($appMock);
+    public function testCreateNodeWithOnlyRequiredConfigUsesTheDefaultValues()
+    {
+        /** @var \Silex\Application||PHPUnit_Framework_MockObject_MockObject $appMock */
+        $appMock = $this->getMockBuilder(Application::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $appMock->expects($this->any())
+            ->method('offsetExists')
+            ->with('manager-config')
+            ->will(
+                $this->returnValue([
+                    'manager' => [
+
+                    ],
+                ]
+                ));
+
+        $appMock->expects($this->any())
+            ->method('offsetGet')
+            ->with('manager-config')
+            ->will(
+                $this->returnValue([
+                    'manager' => [
+                        'dummy' => [
+                            'index' => [
+                                'columns' => []
+                            ],
+                        ],
+                    ],
+                ]));
+
+        $node = new Node($appMock, 'dummy', 'index');
+        $this->assertSame(10, $node->getItemPerPage());
+        $this->assertSame([], $node->getColumns());
+        $this->assertSame('dummy', $node->getDbTable());
+        $this->assertSame('Manager: dummy', $node->getHeader());
+        $this->assertSame('setting', $node->getIcon());
+        $this->assertEmpty($node->getQuery());
+        $this->assertNull($node->getSearch());
+    }
+
+    public function getWrongConfiguration()
+    {
+        return [
+            [
+                [],
+            ],
+            [
+                [
+                    'manager' => [],
+                ],
+            ],
+            [
+                [
+                    'manager' => [
+                        'dummy' => [],
+                    ],
+                ],
+            ],
+            [
+                [
+                    'manager' => [
+                        'dummy' => [
+                            'index' => [],
+                        ],
+                    ],
+                ],
+            ],
+        ];
     }
 }
