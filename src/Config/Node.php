096746575a90c82d3466e1bf8910d7b3b6db9ebc
Jefersson Nathan <admin@phpse.net>
2015-08-11 00:20:29 -0300

    Remove end node commented not existent interface

--- src/Config/Node.php
+++ src/Config/Node.php
@@ -21,7 +21,7 @@ namespace Manager\Config;
 use Manager\Exception\MissingConfigException;
 use Silex\Application;
 
-final class Node // implements ConfigNode
+final class Node
 {
     const ACTION_INDEX = 'index';
 
