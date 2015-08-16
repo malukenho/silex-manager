f9af2325604605841653a2ebd029a9cbb08c533b
Jefersson Nathan <admin@phpse.net>
2015-08-16 05:08:19 -0300

    Remove wrogn file

--- db.sql
+++
@@ -1,7 +0,0 @@
-
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
