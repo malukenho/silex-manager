6c06afc91a2e1990690ef476f95eaca403c3e854
Jefersson Nathan <admin@phpse.net>
2015-08-17 09:26:28 -0300

    Added db DDL example

---
+++ db.sql
@@ -0,0 +1,7 @@
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
